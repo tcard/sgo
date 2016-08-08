tcard/sgo

commit cdf52cca8e50c001c2f097b18181754b4bd4d592

tmp

sgo/types/stmt.go
@@ -27,6 +27,39 @@ func (check *Checker) funcBody(decl *declInfo, name string, sig *Signature, body
 	sig.scope.pos = body.Pos()
 	sig.scope.end = body.End()
 
+	// We need to clone the scope here so that, when changing the type of a
+	// parameter inside the function body (e. g.. when doing `if x != nil { return }`
+	// for optionals), we don't mutate the *Var in its fixed scope, which
+	// would affect further calls to the same function!
+
+	scope := NewScope(sig.scope, sig.scope.pos, sig.scope.end, sig.scope.comment)
+	for _, v := range sig.scope.elems {
+		scope.Insert(v)
+	}
+
+	if sig.params != nil {
+		for _, param := range sig.params.vars {
+			if _, ok := scope.elems[param.name]; ok {
+				delete(scope.elems, param.name)
+			}
+			scope.Insert(NewParam(param.pos, param.pkg, param.name, param.typ))
+		}
+	}
+	if sig.results != nil {
+		for _, param := range sig.results.vars {
+			if _, ok := scope.elems[param.name]; ok {
+				delete(scope.elems, param.name)
+			}
+			scope.Insert(NewParam(param.pos, param.pkg, param.name, param.typ))
+		}
+	}
+	if sig.recv != nil {
+		if _, ok := scope.elems[sig.recv.name]; ok {
+			delete(scope.elems, sig.recv.name)
+		}
+		scope.Insert(NewParam(sig.recv.pos, sig.recv.pkg, sig.recv.name, sig.recv.typ))
+	}
+
 	// save/restore current context and setup function context
 	// (and use 0 indentation at function start)
 	defer func(ctxt context, indent int) {
@@ -35,7 +68,7 @@ func (check *Checker) funcBody(decl *declInfo, name string, sig *Signature, body
 	}(check.context, check.indent)
 	check.context = context{
 		decl:  decl,
-		scope: sig.scope,
+		scope: scope,
 		sig:   sig,
 	}
 	check.indent = 0

commit debe8ba0bb2bf0ff199db7fc52b33d1422a9457c

Fix incorrect "not in scope at return" caused by cdf52cc.

main.go
@@ -1,4 +1,4 @@
-// Autogenerated by SGo revision: cdf52cca8e50c001c2f097b18181754b4bd4d592
+// Autogenerated by SGo revision: 1bd65321f36dd226f242ac0fcc10f6e795ddbdae
 // DO NOT EDIT!
 
 package main

sgo/importer/default.go
@@ -75,25 +75,30 @@ var defaultAnnotations = map[string]map[string]string{
 		"Ticker.C":  `<-chan Time`,
 	},
 	"reflect": {
-		"TypeOf":           `func(interface{}) Type`,
-		"Type.Elem":        `func() Type`,
-		"Type.Key":         `func() Type`,
-		"Value.Interface":  `func() interface{}`,
-		"StructField.Type": `Type`,
+		"TypeOf":            `func(interface{}) Type`,
+		"Type.Elem":         `func() Type`,
+		"Type.Key":          `func() Type`,
+		"Value.Interface":   `func() interface{}`,
+		"Value.Type":        `func() Type`,
+		"StructField.Type":  `Type`,
+		"Type.MethodByName": `func(string) (Method \ bool)`,
 	},
 	"strconv": {
-		"Atoi":      `func(s string) (int \ error)`,
-		"ParseUint": `func(s string, base int, bitSize int) (n uint64 \ err error)`,
-		"ParseInt":  `func(s string, base int, bitSize int) (n int64 \ err error)`,
-		"Unquote":   `func(s string) (t string \ err error)`,
+		"Atoi":       `func(s string) (int \ error)`,
+		"ParseUint":  `func(s string, base int, bitSize int) (n uint64 \ err error)`,
+		"ParseInt":   `func(s string, base int, bitSize int) (n int64 \ err error)`,
+		"ParseFloat": `func(s string, bitSize int) (f float64 \ err error)`,
+		"Unquote":    `func(s string) (t string \ err error)`,
 	},
 	"go/token": {
 		"NewFileSet":         `func() *FileSet`,
 		"(*FileSet).AddFile": `func(filename string, base, size int) *File`,
 	},
 	"go/ast": {
-		"NewScope":       `func(?*Scope) *Scope`,
-		"NewObj":         `func(kind ObjKind, name string) *Object`,
-		"BlockStmt.List": `[]Stmt`,
+		"NewScope":          `func(?*Scope) *Scope`,
+		"NewObj":            `func(kind ObjKind, name string) *Object`,
+		"BlockStmt.List":    `[]Stmt`,
+		"SelectorExpr.Sel":  `Ident`,
+		"CompositeLit.Elts": `[]Expr`,
 	},
 }

sgo/revision.go
@@ -2,4 +2,4 @@
 
 package sgo
 
-var SGoRevision = "cdf52cca8e50c001c2f097b18181754b4bd4d592"
+var SGoRevision = "e1ad4d3437e613919cc7f978f72650bccf02159c"

sgo/types/stmt.go
@@ -441,7 +441,14 @@ func (check *Checker) stmt(ctxt stmtContext, s ast.Stmt) {
 				// list in a "return" statement if a different entity (constant, type, or variable)
 				// with the same name as a result parameter is in scope at the place of the return."
 				for _, obj := range res.vars {
-					if _, alt := check.scope.LookupParent(obj.name, check.pos); alt != nil && alt != obj {
+					// Since we make a copy of the signature scope for the function body, the first
+					// time we find the object in the scope chain will be the copied one. We actually
+					// are interested in the original, so we need to ignore the first result.
+					sc, alt := check.scope.LookupParent(obj.name, check.pos)
+					if sc != nil {
+						sc = sc.parent
+					}
+					if _, alt = sc.LookupParent(obj.name, check.pos); alt != nil && alt != obj {
 						check.errorf(s.Pos(), "result parameter %s not in scope at return", obj.name)
 						check.errorf(alt.Pos(), "\tinner declaration of %s", obj)
 						// ok to continue

sgo/types/api_test.go
@@ -389,7 +389,7 @@ func TestScopesInfo(t *testing.T) {
 			"file:", "func:x y",
 		}},
 		{`package p4; func _(x, y int) { x, z := 1, 2; _ = z }`, []string{
-			"file:", "func:x y z", // redeclaration of x
+			"file:", "func:x y", // redeclaration of x
 		}},
 		{`package p5; func _(x, y int) (u, _ int) { return }`, []string{
 			"file:", "func:u x y",